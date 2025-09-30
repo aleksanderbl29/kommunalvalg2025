dfc81b046a66ed01ed0352f95076c32340245989
aleksanderbl29 <github@aleksanderbl.dk>
2025-09-30 17:37:08 +0200

    Add stantargets example

--- _targets.R
+++ _targets.R
@@ -1,5 +1,6 @@
 library(targets)
 library(tarchetypes)
+library(stantargets)
 
 tar_option_set(
   packages = c("tidyverse", "dkstat", "geodk", "lubridate", "tidylog"),
@@ -9,11 +10,30 @@ tar_option_set(
 )
 tar_source()
 
+generate_data <- function(n = 10) {
+  true_beta <- stats::rnorm(n = 1, mean = 0, sd = 1)
+  x <- seq(from = -1, to = 1, length.out = n)
+  y <- stats::rnorm(n, x * true_beta, 1)
+  list(n = n, x = x, y = y, true_beta = true_beta)
+}
+
 list(
   tar_target(today_date, today("CET")),
   tar_target(mcp_geo, get_mcp_geo(today_date)),
   tar_target(mcp_pop, get_mcp_pop(today_date)),
   tar_target(mcp_accounts, get_mcp_accounts(today_date)),
   tar_target(mcp_daycare_pricing, get_mcp_daycare_pricing(today_date)),
-  tar_target(turnout_pct, get_turnout_pct(today_date))
+  tar_target(turnout_pct, get_turnout_pct(today_date)),
+  tar_stan_mcmc(
+    example,
+    "x.stan",
+    generate_data(),
+    stdout = R.utils::nullfile(),
+    stderr = R.utils::nullfile()
+  ),
+  tar_stan_summary(
+    custom_summary,
+    fit = example_mcmc_x,
+    summaries = list(~posterior::quantile2(.x, probs = c(0.25, 0.75)))
+  )
 )
