a6549b67f14a7735a3c1088b9c0a356d2b31b225
aleksanderbl29 <github@aleksanderbl.dk>
2025-10-09 15:06:11 +0200

    Load valg.dk data and begin historical elections

---
+++ R/valg_dk.R
@@ -0,0 +1,40 @@
+get_kv_election_overview <- function(
+  base_url = "https://valg.dk/api/overview/kv-election-overview"
+) {
+  cookie <- paste0("NSC_mc_wt_wbm_qspe=", paste0(sample(c(0:9, letters[1:6]), 32, replace = TRUE), collapse = ""))
+  headers <- list(
+    "X-Election-ID" = "1705ff7b-7390-48d8-b701-6bcd430dc835",
+    # "Cookie" = "NSC_mc_wt_wbm_qspe=ffffffff090a364345525d5f4f58455e445a4a4229a0"
+    "Cookie" = cookie
+  )
+
+  request(base_url) |>
+    req_headers(!!!headers) |>
+    req_perform() |>
+    resp_body_string() |>
+    jsonlite::fromJSON() |>
+    _$municipalities |>
+    as_tibble() |>
+    mutate(lastUpdate = as_datetime(lastUpdate))
+}
+
+get_kv_data_csv <- function(municipality_id) {
+  base_url <- "https://valg.dk/api/export-data/export-kv-data-csv"
+  election_id <- "1705ff7b-7390-48d8-b701-6bcd430dc835"
+
+  url <- paste0(
+    base_url, "?",
+    "ElectionId=", election_id, "&",
+    "MunicipalityId=", municipality_id |> pull(id)
+  )
+
+  request(url) |>
+    req_perform() |>
+    resp_body_string() |>
+    read_csv2(col_types = cols("c", "c", "c", "c", "d"))
+
+}
+# "https://valg.dk/api/export-data/export-kv-data-csv?
+# ElectionId=1705ff7b-7390-48d8-b701-6bcd430dc835
+# &
+# MunicipalityId=613bbb61-4de7-426d-a1a9-e6ffbaf41140"
--- _targets.R
+++ _targets.R
@@ -4,7 +4,7 @@ library(stantargets)
 library(glue)
 
 tar_option_set(
-  packages = c("tidyverse", "glue", "dkstat", "geodk", "lubridate", "pdftools"),
+  packages = c("tidyverse", "glue", "dkstat", "geodk", "lubridate", "httr2"),
   format = "qs",
   seed = 42,
   controller = crew::crew_controller_local(workers = 4, seconds_idle = 60)
@@ -20,9 +20,19 @@ list(
   tar_target(mcp_accounts, get_mcp_accounts(this_week)),
   tar_target(mcp_daycare_pricing, get_mcp_daycare_pricing(this_week)),
   tar_target(turnout_pct, get_turnout_pct(this_week)),
+
+  # Election results
+  tar_file_read(election_dates, "data/dst/Valg.csv", read_election_dates(!!.x)),
+  tar_file_read(election_results, "data/dst/ValgData.csv", read_election_results(!!.x, election_dates)),
+
+  # Valg.dk
+  tar_group_by(kv_election_overview, get_kv_election_overview(), id),
+  tar_target(kv_data, get_kv_data_csv(kv_election_overview), pattern = map(kv_election_overview)),
+
+  # Polls
   tar_file_read(verian_polls, "data/verian/PI250604.xls", read_verian_excel(!!.x)),
   tar_file_read(gallup_polls, "data/verian/Politisk indeks 1953-2023.xlsx", read_gallup_excel(!!.x)),
-  tar_target(polls, dplyr::bind_rows(verian_polls, gallup_polls)),
+  tar_target(polls, dplyr::bind_rows(verian_polls, gallup_polls))#,
 
   # Calculation of prior
   # tar_target(mcp_deviation, calculate_poll_result_deviation(polls, election_results)),
