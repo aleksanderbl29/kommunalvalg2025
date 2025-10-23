014a5dde0f3fce139dd49431e1fb9013d35813c7
aleksanderbl29 <github@aleksanderbl.dk>
2025-10-23 14:31:16 +0200

    Join properly

--- R/house_effects.R
+++ R/house_effects.R
@@ -18,39 +18,33 @@ calc_house_effects <- function(
 
   municipal_calc <- election_results |>
     group_by(kommune_id, date, party_code) |>
-    summarize(
-      avg_percent = mean(percent, na.rm = TRUE),
-      n = n()
-    ) |>
+    summarize(avg_percent = mean(percent, na.rm = TRUE)) |>
     ungroup() |>
     left_join(polls_filtered, by = join_by(party_code, date == election)) |>
     mutate(poll_delta = avg_percent - value) |>
     group_by(kommune_id, date, party_code, pollster) |>
-    summarize(
-      municipality_delta = mean(poll_delta, na.rm = TRUE),
-      n = n()
-    ) |>
+    summarize(municipality_delta = mean(poll_delta, na.rm = TRUE)) |>
     ungroup() |>
     left_join(parties |> select(party_code, party_name))
 
   valgsted_calc <- election_results |>
     group_by(valgsted, date, party_code) |>
-    summarize(
-      avg_percent = mean(percent, na.rm = TRUE),
-      n = n()
-    ) |>
+    summarize(avg_percent = mean(percent, na.rm = TRUE)) |>
     ungroup() |>
     left_join(polls_filtered, by = join_by(party_code, date == election)) |>
     mutate(poll_delta = avg_percent - value) |>
     group_by(valgsted, date, party_code, pollster) |>
-    summarize(
-      valgsted_delta = mean(poll_delta, na.rm = TRUE),
-      n = n()
-    ) |>
+    summarize(valgsted_delta = mean(poll_delta, na.rm = TRUE)) |>
     ungroup() |>
     left_join(parties |> select(party_code, party_name))
 
   shared_info |>
-    left_join(valgsted_calc, by = join_by(valgsted)) |>
-    left_join(municipal_calc, by = join_by(kommune_id))
+    left_join(
+      valgsted_calc,
+      by = join_by(valgsted)
+    ) |>
+    left_join(
+      municipal_calc,
+      by = join_by(kommune_id, date, party_name, party_code, pollster)
+    )
 }
