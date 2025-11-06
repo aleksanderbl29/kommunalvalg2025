17e4ade8f307d81a3ee9be3709d23425f55d7f52
aleksanderbl29 <github@aleksanderbl.dk>
2025-11-06 20:37:14 +0100

    Prepare some things for a hiearichal model
    its difficult to spell

--- R/model.R
+++ R/model.R
@@ -112,34 +112,84 @@ run_model <- function(prior_draws, polls, election_day) {
 }
 
 run_hierarchical_model <- function(polls, election_day, election_results) {
-  polls
+  new_polls <- polls |>
+    distinct() |>
+    filter(election == election_day) |>
+    mutate(poll_value = as.numeric(value)) |>
+    select(party_code, poll_date, poll_value, segment, pollster) |>
+    distinct() |>
+    pivot_wider(
+      names_from = party_code,
+      values_from = poll_value,
+      values_fill = 0
+    ) |>
+    janitor::clean_names() |>
+    replace_na(list(h = 0)) |>
+    mutate(days_out = poll_date - election_day, area = "nationwide") |>
+    arrange(days_out) %>%
+    mutate(across(
+      c(a, b, c, f, o, v, k, i, o_2, d, a_2, q, h, m, ae, g),
+      ~ ifelse(. == 0, 0.01, .)
+    )) %>%
+    mutate(
+      row_sum = a +
+        b +
+        c +
+        f +
+        o +
+        v +
+        k +
+        i +
+        o_2 +
+        d +
+        a_2 +
+        q +
+        h +
+        m +
+        ae +
+        g,
+      across(
+        c(a, b, c, f, o, v, k, i, o_2, d, a_2, q, h, m, ae, g),
+        ~ . / row_sum
+      ),
+      days_out = as.numeric(days_out) |> abs()
+    ) |>
+    select(-row_sum)
+
+  # new_polls
+  #
+  # election_results
+  #
+  prior_draws |>
+    group_by(party) |>
+    summarise(
+      mean = mean(pred + r_effect),
+      sd = sd(pred + r_effect)
+    )
+
+  priors <- c(
+    # prior(normal(28, 13), class = "Intercept", resp = "a")
+    prior(normal(20, 15), class = "Intercept"),
+    prior(normal(0, 3), class = "b")
+  )
+
+  fit <- brm(
+    cbind(a, b, c, f, o, v, k, i, o_2, d, a_2, q, h, m, ae, g) ~ pollster +
+      segment, #+
+    # (1 | segment),
+    data = new_polls,
+    # prior = priors,
+    family = dirichlet(),
+    cores = 4,
+    chains = 4,
+    backend = "cmdstanr"
+  )
+  # summary(fit)
+  # plot(fit)
+
+  return(fit)
 }
 
-# as_draws_df(prior_draws)
-#
-# as_draws_df(prior_model)
-#
-# x <- gisco_get_communes(year = "2001") |>
-#   filter(CNTR_CODE == "DK")
-# y <- gisco_get_communes(year = "2016") |>
-#   filter(CNTR_CODE == "DK")
-# l <- gisco_get_lau(year = "2021") |>
-#   filter(CNTR_CODE == "DK")
-#
-# nrow(x)
-# nrow(y)
-# nrow(l)
-# x_plot <- x |>
-#   ggplot() +
-#   geom_sf()
-#
-# y_plot <- y |>
-#   filter(CNTR_CODE == "DK") |>
-#   ggplot() +
-#   geom_sf()
-#
-# l_plot <- l |>
-#   filter(CNTR_CODE == "DK") |>
 
 #   ggplot() +
 #   geom_sf()
